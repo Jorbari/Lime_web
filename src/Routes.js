f99b2789d54b2a3c5c1e208e99346926b6f40790
nedyudombat <nedyudombat@gmail.com>
2020-11-02 03:24:24 +0100

    surveys

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import AuthWrapper from "./views/AuthWrapper/AuthWrapper";
 import Dashboard from "./views/Dashboard/Dashboard";
 import Projects from "./views/Projects/Projects";
 import Project from "./views/Projects/Project";
+import SingleSurvey from "./views/Surveys/SingleSurvey";
 import Surveys from "./views/Surveys/Surveys";
 import Report from "./views/Report/Report";
 import Profile from "./views/Profile/Profile";
@@ -26,6 +27,7 @@ const Routes = () => {
           <ProtectedRoute exact path="/projects" component={Projects} />
           <ProtectedRoute exact path="/projects/:id" component={Project} />
           <ProtectedRoute exact path="/surveys" component={Surveys} />
+          <ProtectedRoute exact path="/surveys/:id" component={Surveys} />
           <ProtectedRoute exact path="/report" component={Report} />
           <ProtectedRoute exact path="/profile" component={Profile} />
         </Switch>
