bc8c5dd287cd6499cd617b53490588a29b22eba6
victory-oki <victorysplogi@gmail.com>
2020-11-23 23:03:11 +0100

    fix button styles on new-survey page

--- src/views/Surveys/survey-new/survey-new.styles.jsx
+++ src/views/Surveys/survey-new/survey-new.styles.jsx
@@ -31,3 +31,12 @@ export const Caret = styled(caret)`
     pointer-events: none;
     cursor: pointer;
 `
+
+export const ButtonGroup = styled.div`
+    padding: 3rem 0;
+    display: flex;
+    align-items: center;
+    & >:not(:last-child){
+        margin-right: 3rem;
+    }
+`
