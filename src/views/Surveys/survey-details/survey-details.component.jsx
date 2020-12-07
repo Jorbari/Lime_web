5c8dd084c38248a229b2ed2c8865e5d196c730e5
Jorbari <jorbarinaaton1@gmail.com>
2020-12-07 15:35:02 +0100

    design survey collector component

--- src/views/Surveys/survey-details/survey-details.component.jsx
+++ src/views/Surveys/survey-details/survey-details.component.jsx
@@ -1,72 +1,60 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { withRouter } from 'react-router-dom';
-import { SapsProjectContainer } from './survey.details.styles';
-import SurveySummary from '../survey-summary/survey-summary';
+import { withRouter } from "react-router-dom";
+import { SapsProjectContainer } from "./survey.details.styles";
+import SurveySummary from "../survey-summary/survey-summary";
+import SurveyCollector from "../survey-collectors/survey-collectors.component";
 
 class SurveyDetails extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      currentSurveyId: this.props.match.params.id,
+    };
+  }
 
-        this.state = {
-            currentSurveyId: this.props.match.params.id
-        }
-    }
+  render() {
+    return (
+      <SapsProjectContainer>
+        <div className="content__wrapper">
+          <div className="flex__right">
+            <button>Record response </button>
+            <button
+              onClick={() =>
+                this.props.history.push(
+                  `/surveys/edit/${this.state.currentSurveyId}`
+                )
+              }
+            >
+              Edit
+            </button>
+            <button>Analyse</button>
+          </div>
 
-    render() {
+          <Tabs>
+            <TabList className="tab">
+              <Tab className="tab__list">Summary</Tab>
+              <Tab className="tab__list">Preview</Tab>
+              <Tab className="tab__list">Collectors</Tab>
+              <Tab className="tab__list">Analyse</Tab>
+            </TabList>
 
-        return (
-            <SapsProjectContainer>
-        
-                <div className="content__wrapper">
-        
-                    <div className="flex__right">
-                        <button>Record response </button>
-                        <button onClick={ () => this.props.history.push(`/surveys/edit/${this.state.currentSurveyId}`) } >Edit</button>
-                        <button>Analyse</button>
-                    </div>
-        
-                    <Tabs>
-                    <TabList className="tab">
-                      <Tab className="tab__list">
-                        Summary
-                      </Tab>
-                      <Tab className="tab__list">
-                        Preview
-                      </Tab>
-                      <Tab className="tab__list">
-                        Collectors
-                      </Tab>
-                      <Tab className="tab__list">
-                        Analyse
-                      </Tab>
-                    </TabList>
-        
-                    <TabPanel>
-                        <SurveySummary />
-                    </TabPanel>
-                    <TabPanel>
-                    <h1>Hello two</h1>
-                    </TabPanel>
-                    <TabPanel>
-                    <h1>Hello three</h1>
-                    </TabPanel>
-                    <TabPanel>
-                    <h1>Hello four</h1>
-                    </TabPanel>
-                  </Tabs>
-        
-                </div>
-                
-            </SapsProjectContainer>
-        
-          );
-
-    }
-
- 
-};
-
+            <TabPanel>
+              <SurveySummary id={this.state.currentSurveyId} />
+            </TabPanel>
+            <TabPanel>hello two</TabPanel>
+            <TabPanel>
+              <SurveyCollector />
+            </TabPanel>
+            <TabPanel>
+              <h1>Hello four</h1>
+            </TabPanel>
+          </Tabs>
+        </div>
+      </SapsProjectContainer>
+    );
+  }
+}
 
 export default withRouter(SurveyDetails);
--- src/views/Surveys/survey-summary/survey-summary.jsx
+++ src/views/Surveys/survey-summary/survey-summary.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import moment from "moment";
 import { SurveyComponent } from './survey-summary.styles';
+import { getSingleSurveyRequest } from '../../../api/survey';
 
 class SurveySummary extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            surveySummary: {}
+        }
+
+        this.getSurveySummary();
        
+    }
+
+    async getSurveySummary () {
+        let res = await getSingleSurveyRequest(this.props.id);
+        if (res.status === 200) {
+            this.setState({surveySummary: res.data.data});
+            console.log(this.state.surveySummary);
+        }
+        console.log(res.data.data);
     }
 
     render() {
@@ -16,11 +33,11 @@ class SurveySummary extends React.Component {
                     <div className="section__one">
                         <section>
                             <h3>Total No. of Responses</h3>
-                            <p>9</p>
+                            <p> {this.state.surveySummary?.numberOfResponses || 0} </p>
                         </section>
                         <section>
                             <h3>Survey Status</h3>
-                            <p>Active</p>
+                            <p> {this.state.surveySummary?.status || '---'} </p>
                         </section>
                         <section>
                             <h3>Drafted Surveys:</h3>
@@ -37,19 +54,19 @@ class SurveySummary extends React.Component {
 
                                 <section className="details">
                                     <p>Projet:</p>
-                                    <h4>SAPS Project</h4>
+                                    <h4> {this.state.surveySummary?.project || '---'} </h4>
                                 </section>
                                 <section className="details">
                                     <p>No. of questions:</p>
-                                    <h4>12</h4>
+                                    <h4> {this.state.surveySummary?.numberOfQuestions || '0'} </h4>
                                 </section>
                                 <section className="details">
                                     <p>Date Started:</p>
-                                    <h4>10th of January 2020</h4>
+                                    <h4> {moment(this.state.surveySummary?.dateStarted).format("Do of MMMM YYYY") || '---'} </h4>
                                 </section>
                                 <section className="details">
                                     <p>Last Modified:</p>
-                                    <h4>14th of January 2020</h4>
+                                    <h4> {moment(this.state.surveySummary?.dateUpdated).format("Do of MMMM YYYY") || '---'} </h4>
                                 </section>
                                 <section className="details">
                                     <p>Estimated completion time</p>
@@ -72,7 +89,7 @@ class SurveySummary extends React.Component {
                                     </section>
                                     <section></section>
                                     <section>
-                                        <h4>Live Survey</h4>
+                                        <h4>Web Link</h4>
                                         <p>3 responses</p>
                                     </section>
 
