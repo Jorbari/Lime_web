1e1a18e681ef11b52397ea730d2bbd98f90ef017
Jorbari <jorbarinaaton1@gmail.com>
2020-12-21 17:49:49 +0100

    modify some designs

--- src/api/user.js
+++ src/api/user.js
@@ -2,3 +2,6 @@ import { http } from "./client";
 
 export const updateUserDetails = async (userInfo) =>
   await http.put("/api/user", userInfo);
+
+export const updateUserProfileImage = async (imageBinary) =>
+  await http.post("/api/upload", imageBinary);
